5631f13fc6736e991d0db120c446abe60d7cce43
josybarzola <joselynbarzola16@gmail.com>
2023-10-22 17:22:10 -0500

    Clases

---
+++ src/main/java/ec/edu/espol/tareaaop/Cilindro.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tareaaop;
+
+/**
+ *
+ * @author USER
+ */
+public class Cilindro {
+    private double radio; 
+
+    public Cilindro(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+    
+}
---
+++ src/main/java/ec/edu/espol/tareaaop/TrianguloEquilateroPrisma.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tareaaop;
+
+/**
+ *
+ * @author USER
+ */
+public class TrianguloEquilateroPrisma {
+    private double lado; 
+
+    public TrianguloEquilateroPrisma(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+    
+}
